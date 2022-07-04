7b47f3fdcf21f28d3bcd77a669e1f023193ad268
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-04 18:18:51 +0530

    Added ElectionResultView

---
+++ src/view/molecules/ElectionResultView.js
@@ -0,0 +1,13 @@
+import PctSlider from "../../view/atoms/PctSlider"
+
+export default function ElectionResultView({electionResult}) {
+  console.debug(electionResult);
+  return (
+    Object.entries(electionResult.edPctIdx).map(
+      function([edID, pctVotes]) {
+        return <PctSlider key={"pct-slider_" + edID} edID={edID} pctVotes={pctVotes} />;
+      }
+    )
+
+  );
+}
