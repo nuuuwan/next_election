d1945e4dfc19318b326f8a2dc6cb2238cf9acb32
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-04 18:19:10 +0530

    Use ElectionResultView

--- src/view/pages/DashboardPage.js
+++ src/view/pages/DashboardPage.js
@@ -1,7 +1,12 @@
 import { Component } from "react";
+import {ELECTION_RESULT_IDX} from "../../nonview/core/ElectionResult"
+import ElectionResultView from "../../view/molecules/ElectionResultView"
+
 
 export default class DashboardPage extends Component {
   render() {
-    return "TODO-DashboardPage";
+    const electionResult = ELECTION_RESULT_IDX[2020]['SJB'];
+
+    return <ElectionResultView electionResult={electionResult} />;
   }
 }
