203941b8aca3520656545c75c49c889f6c6863a1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-05 15:48:06 +0530

    Use constructEmpty

--- src/view/pages/DashboardPage.js
+++ src/view/pages/DashboardPage.js
@@ -2,20 +2,16 @@ import { Component } from "react";
 
 import Stack from "@mui/material/Stack";
 
-import { YEAR_TO_PARTY_TO_ELECTION_RESULT } from "../../nonview/core/ElectionResult";
+import ElectionResult from "../../nonview/core/ElectionResult";
 
 import ElectionResultView from "../../view/molecules/ElectionResultView";
 import GroupsView from "../../view/molecules/GroupsView";
 import NationalView from "../../view/molecules/NationalView";
 
-const DEFAULT_YEAR = 2020;
-const DEFAULT_PARTY = "SJB";
-
 export default class DashboardPage extends Component {
   constructor(props) {
     super(props);
-    const electionResult =
-      YEAR_TO_PARTY_TO_ELECTION_RESULT[DEFAULT_YEAR][DEFAULT_PARTY];
+    const electionResult = ElectionResult.constructEmpty();
     this.state = { electionResult };
   }
 
