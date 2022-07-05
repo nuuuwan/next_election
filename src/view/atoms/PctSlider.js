093a2418bc98e4360feaa7a8b88f223a346abe4b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-05 12:41:09 +0530

    Removed non-generic

--- src/view/atoms/PctSlider.js
+++ src/view/atoms/PctSlider.js
@@ -1,14 +1,6 @@
 import Slider from "@mui/material/Slider";
 
-const WIDTH_PER_SEAT = 15;
-
-export default function PctSlider({
-  edId,
-  partyPct,
-  totalSeats,
-  onChangePartyPct,
-  color,
-}) {
+export default function PctSlider({ partyPct, onChangePartyPct, color }) {
   const onChangePartyPctInner = function (e) {
     if (onChangePartyPct) {
       onChangePartyPct(e.target.value);
@@ -22,7 +14,7 @@ export default function PctSlider({
       value={partyPct}
       step={0.001}
       onChange={onChangePartyPctInner}
-      sx={{ width: totalSeats * WIDTH_PER_SEAT, color }}
+      sx={{ color }}
     />
   );
 }
