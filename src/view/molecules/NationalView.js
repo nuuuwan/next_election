8a56bfed0ba89d90f4103e4f78b5ce0517a02892
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-06 09:12:06 +0530

    Added Card

--- src/view/molecules/NationalView.js
+++ src/view/molecules/NationalView.js
@@ -1,15 +1,13 @@
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
-import SeatView from "../../view/atoms/SeatView"
+import SeatView from "../../view/atoms/SeatView";
 
 export default function NationalView({ electionResult }) {
   const totalSeats = electionResult.getTotalSeats();
   return (
-    <Card sx={{ m: 1, p: 2, width: 140 }}>
-      <Typography variant="h4">
-        {totalSeats} Seats
-      </Typography>
-      <SeatView totalSeats={totalSeats}/>
+    <Card sx={{ m: 2, p: 2, width: 250 }}>
+      <Typography variant="h4">{totalSeats} Seats</Typography>
+      <SeatView totalSeats={totalSeats} />
     </Card>
   );
 }
