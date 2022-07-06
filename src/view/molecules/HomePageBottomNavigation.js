67d304678796b35f55cf3acf7759891a9449f4cf
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-06 12:58:36 +0530

    Added Clear

--- src/view/molecules/HomePageBottomNavigation.js
+++ src/view/molecules/HomePageBottomNavigation.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
+import ClearIcon from "@mui/icons-material/Clear";
 
-export default function HomePageBottomNavigation({ onClickOpenPage }) {
+export default function HomePageBottomNavigation({ onClickClear }) {
   return (
     <Paper
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
       elevation={3}
     >
-      <BottomNavigation></BottomNavigation>
+      <BottomNavigation>
+        <BottomNavigationAction icon={<ClearIcon />} onClick={onClickClear} />
+      </BottomNavigation>
     </Paper>
   );
 }
